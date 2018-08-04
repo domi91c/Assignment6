0c9897140d016304b6c9af9e27136547276edc47
domi91c <dominic.n@me.com>
2018-08-03 22:07:58 -0400

    created public directory and moved angular app files there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 /************************************************************************ *********
- * WEB422 – Assignment 05
+ * WEB422 – Assignment 06
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students. *
- * Name: Dominic Nunes Student ID: 016-183-121 Date: July 20, 2018
+ * Heroku Link: https://web422-assignment6.herokuapp.com/
+ * Name: Dominic Nunes Student ID: 016-183-121 Date: August 3, 2018
  * ********************************************************************************/
 
 import {Component} from '@angular/core';
